ce19a2bdd61da6ce83bdd023bc76b78a4b01bf22
ShawonAkash <mdshawonakash@gmail.com>
2021-12-06 20:02:00 +0600

    1st

---
+++ src/views/EditProject.vue
@@ -0,0 +1,51 @@
+<template>
+  <form @submit.prevent="handleUpdate">
+    <label for="">Title:</label>
+    <input type="text" v-model="title" required>
+    <label for="">Details</label>
+    <textarea v-model="details" required></textarea>
+    <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+ data(){
+   return{
+     title: '',
+     details: '',
+     uri: 'http://localhost:3000/projects/' +this.id
+   }
+ },
+ mounted(){
+   fetch(this.uri)
+   .then(res => res.json())
+   .then(data => {
+     this.title = data.title
+     this.details = data.details
+   })
+ },
+ methods:{
+   handleUpdate(){
+     let project = {
+        title: this.title,
+        details: this.details,
+        complete: false
+      }
+       
+       fetch(this.uri, {
+         method: 'PATCH',
+         headers: {'Content-Type': 'application/json'},
+         body: JSON.stringify(project)
+       }).then(() => {
+         this.$router.push('/')
+       }).catch((err) => console.log(err))
+   }
+ }
+}
+</script>
+
+<style>
+
+</style>
